8f046ae27e4c48c87b764e5b92955717bfc117d0
milan <milan.naik@happiestminds.com>
2024-04-30 12:31:27 +0530

    added multiplication feature

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from calc_func import do_addition , do_subtraction
+from multiply import do_multiply
 def main():
     print('welcome to the calculator app')
     print("""\n select the function from the given options
           1. Add
           2. Subtract
+          3. Multiply
           """)
     user_input = input("select the function")
 
@@ -14,6 +16,8 @@ def main():
         result = do_addition(a,b)
     elif user_input == "2":
         result = do_subtraction(a,b)
+    elif user_input =="3":
+        result = do_multiply(a,b)
 
     print('Result is ',result)
 
